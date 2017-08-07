727eb58aff965eb5fb07e0971e2b67ead81c0b2d
lifengleaf <lifengleaf@gmail.com>
2017-08-07 12:17:55 -0700

    refactor to use jQuery

--- mario.js
+++ mario.js
@@ -1,6 +1,5 @@
 
-const pyramid = document.getElementById("pyramid");
-const error = document.querySelector(".error-message");
+const error = $(".error-message");
 
 var filterInt = function(value) {
   if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
@@ -8,9 +7,7 @@ var filterInt = function(value) {
   return NaN;
 }
 
-
-button = document.getElementById('draw-button');
-button.onclick = function(event){
+$('#draw-button').click(function(event){
     // prevent the form from submitting
     event.preventDefault();
 
@@ -19,37 +16,40 @@ button.onclick = function(event){
     //clear previous error messages
     error.innerHTML = "";
 
-    heightStr = document.getElementById("height").value;
+    heightStr = $("#height").val();
     // check if the input is valid integer
-    height = filterInt(heightStr);
+    heightInt = filterInt(heightStr);
 
-    if(!height){  
-        error.innerHTML = "Please provide a positive integer.";
+    if(!heightInt){  
+        error.text("Please provide a positive integer.");
     }
-    else if (height > 100){
-        error.innerHTML = "The pyramid is too tall to show.";
+    else if (heightInt > 100){
+        error.text("The pyramid is too tall to show.");
     }
     else {
-        drawPyramid(height);
+        drawPyramid(heightInt);
     }
-}
+});
 
 
 function drawPyramid(height) {
     for (let r = 0; r < height; r++) {
-        let row = document.createElement("div");
-        row.className = "row";
+        let row = $("<div>", {
+            "class": "row"
+        });
         for (let c = 0; c < height-r-1; c++){
-            let cell = document.createElement("div");
-            cell.className = "cell-hide";
-            row.appendChild(cell);
+            let cell = $("<div>", {
+            "class": "cell-hide"
+        });
+            row.append(cell);
         }        
         for (let c = 0; c < r+2; c++){
-            let cell = document.createElement("div");
-            cell.className = "cell";
-            row.appendChild(cell);
+            let cell = $("<div>", {
+            "class": "cell"
+        });
+            row.append(cell);
         }
-        pyramid.appendChild(row);       
+        $("#pyramid").append(row);       
     }  
 }
 
